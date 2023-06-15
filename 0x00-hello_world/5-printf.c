88a665792cdb02d8fa288fd224559bf77c82a20d
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-06-15 08:09:57 -0700

    print a sentence with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - using printf to print a sentence
+ * Return: 0 always.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
