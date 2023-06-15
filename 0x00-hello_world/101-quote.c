da06b1c8c30dc0825fa96146e829902b9254c822
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-06-15 10:10:04 -0700

    printing to the error standard

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <unistd.h>
+/**
+ * main - writes sentence to the standard error
+ * Return: Always (1)
+ */
+
+
+
+
+int main(void)
+{
+
+	const char *words;
+
+	words = "and that piece of art  is useful\" - Dora Korpar\
+, 2015-10-19\n";
+
+	write(2, words, strlen(words));
+	return (1);
+}
