6a418788534af325c4be18a80f82f186dd0fa1ca
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-06-20 00:06:29 -0700

    printing _putchar from another c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+/**
+ * main - writes _putchar to stdout
+ * _putchar - writes char to stdout
+ * @c : character to be written
+ * Return: 0 always
+ */
+
+int _putchar(char c);
+
+int main(void)
+	{
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
+
+		return (0);
+	}
+
