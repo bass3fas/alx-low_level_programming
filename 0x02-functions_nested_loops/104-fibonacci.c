06d4451b331bae19ccdc9b08b400409772b1792d
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-06-21 11:18:28 -0700

    struct to learn

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,19 +5,50 @@
  */
 
 
+typedef struct
+{
+	unsigned long high;
+	unsigned long low;
+}DUL;
+
+void print_dul(DUL num)
+{
+	if (num.high != 0)
+		printf("%lu%016%lu", num.high, num.low);
+	else
+		printf("%lu", num.low);
+}
+
+DUL add_dul(DUL a, DUL b)
+{
+	DUL result;
+
+	result.low = a.low + b.low;
+	result.high = a.high + b.high + (result.low < a.low);
+
+	return (result);
+}
+
+
 int main(void)
 {
-	unsigned long i, next, first, second;
+	unsigned int count;
+	DUL first, second, next;
 
-	first = 1;
-	second = 2;
+	first = { 0, 1 };
+	second = { 0, 2 };
 
-	printf("%lu, %lu", first, second);
 
-	for (i = 3; i <= 98; i++)
+	print_dul(first);
+	printf(", ");
+	print_dul(second);
+
+
+	for (count = 3; count <= 98; count++)
 	{
-		next = first + second;
-		printf(", %lu", next);
+		next = add_dul(first, second);
+		print_dul(next);
+		printf(", ");
 
 		first = second;
 		second = next;
