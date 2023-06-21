4247bb05eaacebc12347ebc8c50d76ac53fefaca
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-06-21 10:50:16 -0700

    fibo 98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - print fibonacci
+ * Return: success
+ */
+
+
+int main(void)
+{
+	unsigned long i, next, first, second;
+
+	first = 1;
+	second = 2;
+
+	printf("%lu, %lu", first, second);
+
+	for (i = 3; i <= 98; i++)
+	{
+		next = first + second;
+		printf(", %lu", next);
+
+		first = second;
+		second = next;
+	}
+	printf("\n");
+
+	return (0);
+}
