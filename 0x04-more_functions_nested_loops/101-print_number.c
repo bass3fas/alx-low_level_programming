528104b114eddaf591548eb7188c34b182eee66d
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-06-22 13:21:35 -0700

    print

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_number - go
+ * @n: number
+ */
+
+
+void print_number(int n)
+{
+	int a, b;
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	while (n / 10 != 0)
+	{
+		_putchar('0' + n % 10)
+			n = n / 10;
+	}
+	_putchar('\n');
+}
+
