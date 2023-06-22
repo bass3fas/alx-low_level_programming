078e4c3ffc5ec4094714bdb1f74bfeff55f6390c
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-06-22 09:29:22 -0700

    test 5 #1

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - is whatever
+ */
+
+void more_numbers(void)
+{
+	int c, d;
+
+	for (c = 0; c <= 10; c++)
+	{
+		for (d = 0; d <= 14; d++)
+		{
+			_putchar('0' + d / 10);
+			_putchar('0' + d % 10);
+		}
+		_putchar('\n');
+	}
+}
