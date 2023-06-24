37e004230845c9c5ccf088c71b64d8eeb8c84267
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-06-24 01:14:39 -0700

    fib

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,22 +7,45 @@
 
 int main(void)
 {
-	long int count;
-	long int first, second, next;
+	int count;
+	unsigned long int first, second, firstl, secondl;
+	unsigned long int next, nextl, nextls, largest;
 
+
+	firstl = 0;
+	secondl = 0;
 	first = 1;
 	second = 2;
+	largest = 10000000000;
 
-	printf("%ld, %ld, ", first, second);
+	printf("%lu, %lu, ", first, second);
 
 
 	for (count = 3; count <= 98; count++)
 	{
-		next = first + second;
-		printf("%ld, ", next);
+		if (first + second > largest || firstl > 0 || secondl > 0)
+		{
+			nextl = (firstl + secondl) / largest;
+			next = (first + second) % largest;
+			nextls = firstl + secondl + nextl;
+			firstl = secondl;
+			secondl = nextls;
+			first = second;
+			second = next;
+			printf("%lu, ", nextl, next);
+		}
 
-		first = second;
-		second = next;
+		else
+		{
+
+			next = first + second;
+			first = second;
+			second = next;
+			printf("%lu", next);
+		}
+
+		if (count < 98)
+			printf(", ");
 	}
 	printf("\n");
 
