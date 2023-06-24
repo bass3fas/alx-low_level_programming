5c8e8803cba13cbc3a2612f0289d824399c08721
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-06-24 09:10:26 -0700

    print any number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,13 +7,26 @@
 
 void print_number(int n)
 {
+	int c, m; /* counter of 10 powers, power value */
+
+	c = 0;
+	m = 1;
+
 	if (n < 0)
 	{
 		_putchar(45);
 		n = n * -1;
 	}
-
-	if (n / 10)
-		print_number(n / 10);
-	_putchar(48 + n % 10);
+	while (n / m != 0)
+	{
+		m = m * 10;
+		c = c + 1;
+	}
+	while (m > 10)
+	{
+		m = m / 10;
+		_putchar('0' + n / m);
+		n = n % m;
+	}
+	_putchar('0' + n);
 }
