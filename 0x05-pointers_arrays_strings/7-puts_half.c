56cbdb05086e8818991841c2040813b5a54b839c
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-06-26 11:41:33 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * puts-half - puts
+ * @str: n
+ */
+
+void puts_half(char *str)
+{
+	int c, l;
+
+	for (c = 0; str[c] != '\0'; c++)
+		;
+	c++;
+
+	l = c;
+
+	for (c = l / 2; str[c] != '\0'; c++)
+		_putchar(str[c]);
+
+	_putchar('\n');
+}
