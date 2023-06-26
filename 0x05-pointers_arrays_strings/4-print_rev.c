15329f5cea7d847fd0a7d0b63bb750fce7b0215d
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-06-26 10:46:36 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * print_rev - rev
+ * @c: cha
+ */
+
+void print_rev(char *str)
+{
+	int c;
+
+	for (c = 0; str[c] != '\0'; c++)
+		;
+	for (c--; c >= 0; c--)
+		_putchar(str[c]);
+
+	_putchar('\n');
+}
