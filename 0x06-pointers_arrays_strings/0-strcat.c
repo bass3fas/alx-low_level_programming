a165270a7423985bc838a90cf7136d0812bf45bd
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-07-01 10:19:21 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,18 @@
 #include "main.h"
 /**
- * - 
- * @ 
- * Return:
+ * _strcat - does something
+ * @dest: var
+ * @src: var
+ * Return: char
  */
+char *_strcat(char *dest, char *src)
+{
+	int i, k;
 
-
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (k = 0; src[k] != '\0'; k++)
+		dest[i + k] = src[k];
+	dest[i + k] = '\0';
+	return (dest);
+}
