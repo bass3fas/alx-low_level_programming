df1c7a444da68231d03369b52b022cf1d820b358
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-07-01 11:43:08 -0700

    strncat 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,7 +12,12 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; dest[i] != '\0'; i++)
 		;
-	for (k = 0; k < n && src[k] != '\0'; k++)
-		    dest[i + k] = src[k];
+	k = 0;
+	while (k < n && src[k] != '\0')
+	{
+		dest[i + k] = src[k];
+		k++;
+	}
+	dest[i + k] = '\0';
 	return (dest);
 }
