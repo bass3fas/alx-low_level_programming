5602ca04f03998b56a76f96d99bc0e3a92ae538d
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-07-01 12:44:40 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,8 +1,15 @@
 #include "main.h"
 /**
- * - 
- * @ 
- * Return:
+ * _strcmp - does something
+ * @s1: s
+ * @s2: s
+ * Return: diff
  */
+int _strcmp(char *s1, char *s2)
+{
+	int r;
 
+	r = s1 - s2;
+	return (r);
+}
 
