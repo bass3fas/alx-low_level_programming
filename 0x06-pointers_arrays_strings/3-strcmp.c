ea49beffcfcedea0854d7bfea479d6c4853d86e7
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-07-01 13:11:17 -0700

    cmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,9 +7,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int r;
+	int r, i;
 
-	r = *s1 - *s2;
+	r = 0;
+	for(i = 0; s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]; i++)
+		;
+	r = s1[i] - s2[i];
 	return (r);
 }
 
