837591fb415d43c4592cc632eea5eb77ce671813
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-07-05 12:05:47 -0700

    oleee

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * natural_square - something
+ * @n: var
+ * @i: counter
+ * Return: i
+ */
+int natural_square(int n, int i)
+{
+	if (i * i >= n)
+		return (i);
+	return (natural_square(n, i + 1));
+}
+/**
+ * iterate - something
+ * @n: var
+ * @i: counter
+ * Return: 1 or 0
+ */
+int iterate(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0  && i > 1)
+                return (0);
+	return (iterate(n, i - 1));
+}
+
+/**
+ * is_prime_number - something
+ * @n: var
+ * Return: n
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	return (iterate(n, natural_square(n, 0)));
+}
