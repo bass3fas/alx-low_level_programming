4ea84246ad837da724930f4a6e8e81f38907aa44
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-07-05 12:53:45 -0700

    trial

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,9 +8,9 @@ int is_palindrome(char *c)
 {
 	if(*c)
 	{
-		if (*c == *(c + _strlen_recursion(c))
-		    is_palindrome(c + 1);
-		    return (0);
+		if (*c == *(c + _strlen_recursion(c)))
+			is_palindrome(c + 1);
+		return (0);
 	}
 		return (1);
 }
