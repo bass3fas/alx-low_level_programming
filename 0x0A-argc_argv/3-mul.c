5e116d979efe4a2b2c6ba9100003e64667e6023d
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-07-08 15:15:27 -0700

    atoi

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,50 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ * _atoi - string to int
+ * @s: string
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+/**
  * main - prints mul of arg of the file
  * @argc: count of arg
  * @argv: vector of arg
@@ -15,8 +59,8 @@ int main(int argc, char *argv[])
 		printf("ERROR\n");
 		return (1);
 	}
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[2]);
+	n1 = _atoi(argv[1]);
+	n2 = _atoi(argv[2]);
 	mul = n1 * n2;
 	printf("%d\n", mul);
 	return (0);
