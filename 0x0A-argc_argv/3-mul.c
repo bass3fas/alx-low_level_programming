e7f219d684aad09d538d46b1fa34cccda3390247
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-07-08 12:58:21 -0700

    edit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,15 +8,16 @@
  */
 int main(int argc, char *argv[])
 {
-	int mul;
+	int n1, n2, mul;
 
 	if (argc != 3)
 	{
 		printf("ERROR\n");
 		return (1);
 	}
-
-	mul = atoi(argv[1]) * atoi(argv[2]);
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	mul = n1 * n2;
 	printf("%d\n", mul);
 	return (0);
 }
