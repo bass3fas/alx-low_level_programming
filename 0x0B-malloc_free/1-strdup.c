5e6be6c36f018595167a97f12be11a40d2925bf8
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-07-11 15:49:24 -0700

    1 -2

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,6 +2,25 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+/**
  * _strdup - creat array
  * @str: var
  * Return: char
@@ -11,7 +30,7 @@ char *_strdup(char *str)
 	char *p;
 	unsigned int i, len;
 
-	len = strlen(str);
+	len = _strlen(str);
 	p = malloc(sizeof(char) * (len + 1));
 
 	if (p == NULL || str == NULL)
