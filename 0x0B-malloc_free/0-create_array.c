98665d7d5a9d9fea729cb924a58d650aead00173
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-07-11 15:19:57 -0700

    unsigned

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,7 +9,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *p;
-	int i;
+	unsigned int i;
 
 	p = malloc(sizeof(char) * size);
 
@@ -17,5 +17,7 @@ char *create_array(unsigned int size, char c)
 	{
 		p[i] = c;
 	}
+	if (p == NULL || size == 0)
+		return (NULL);
 	return(p);
 }
