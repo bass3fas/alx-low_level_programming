96eaa7f5a4e7a73149f4ec38fd078ef7a76233d6
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-07-22 23:46:40 -0700

    3

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - do
+ * @size: g
+ * @cmp: t
+ * @array: y
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	if (!array || !cmp)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+}
