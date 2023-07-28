16ec28700e30c2c55d5f21894f289726c651ba62
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-07-28 06:23:37 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_list - print
+ * @h: head
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	const struct list_t *current = h;
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", current->len, current->str);
+		current = current->next;
+		count++;
+	}
+	return (count);
+}
