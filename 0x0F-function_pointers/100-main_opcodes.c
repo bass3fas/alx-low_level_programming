f9b7eb0a468b1afd366ad9f554e885881d5df22f
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-07-29 00:39:10 -0700

    4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,18 +11,18 @@ int main(int argc, char *argv[])
 	int bytes, i;
 	char *arr;
 
-	arr = (char *)main;
-	bytes = atoi(argv[1]);
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
+	bytes = atoi(argv[1]);
 	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
+	arr = (char *)main;
 	for (i = 0; i < bytes; i++)
 	{
 		if (i == bytes - 1)
