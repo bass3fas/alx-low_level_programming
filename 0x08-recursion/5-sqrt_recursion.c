f8e68e38b288a872fc4d008f430712543b721019
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-08-02 11:13:43 -0700

    fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,12 +7,12 @@
  */
 int _sqrt(int n, int i)
 {
-	if (i <= 0)
+	if (i * i > n)
 		return (-1);
 	else if (i * i == n)
 		return (i);
 	else
-		return (_sqrt(n, i - 1));
+		return (_sqrt(n, i + 1));
 }
 /**
  * _sqrt_recursion - something
@@ -21,7 +21,7 @@ int _sqrt(int n, int i)
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0 )
+	if (n < 0)
 		return (-1);
-	return (_sqrt(n, n - 1));
+	return (_sqrt(n, 0));
 }
