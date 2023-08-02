285d5ff01c9556fe3f4b4a3f31cae477781160d4
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-08-02 09:21:49 -0700

    strlen #2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,13 +6,8 @@
  */
 int _strlen_recursion(char *s)
 {
-	int i = 0;
-
 	if (*s != '\0')
-	{
-		i++;
-		_strlen_recursion(s + 1);
-	}
+		return ( 1 + _strlen_recursion(s + 1));
 	else
-	return (i);
+		return (0);
 }
