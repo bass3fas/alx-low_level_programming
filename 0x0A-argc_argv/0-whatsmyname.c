6604410358b951a80cd16e36bb2ffadac393645e
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-08-03 23:10:23 -0700

    file name printing

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - some
+ * @argc: var
+ * @argv: arr
+ * Return: success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
