92d426b5763b6477d1de1f795449cd955a032459
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-08-07 12:10:26 -0700

    free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,6 +1,15 @@
 #include "main.h"
 /**
- * - does something
- * @ : 
- * Return: 
+ * free_grid - does something
+ * @grid: var
+ * @height: var
  */
+void free_grid(int **grid, int height)
+{
+	int i;
+	if (grid == NULL || height == 0)
+		return;
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
