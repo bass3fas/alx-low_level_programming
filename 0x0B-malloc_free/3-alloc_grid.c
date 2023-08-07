57062f0602e62eb7777457801b45c197d521769e
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-08-07 12:03:46 -0700

    grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,6 +1,35 @@
 #include "main.h"
 /**
- * - does something
- * @ : 
- * Return: 
+ * alloc_grid - does something
+ * @width : var
+ * @height: var
+ * Return: int
  */
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int i,j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	array = (int **)malloc(sizeof(int *) * height);
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		array[i] = (int *)malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+		{
+			free(array);
+			for (j = 0; j<= i; j++)
+				free(array[j]);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			array[i][j] = 0;
+	}
+	return (array);
+}
