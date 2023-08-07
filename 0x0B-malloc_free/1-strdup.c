7ea70978298c342759b748d89374a8af4e816ec1
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-08-07 02:01:22 -0700

    string duplication task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,26 @@
 #include "main.h"
 /**
- * - does something
- * @ : 
- * Return: 
+ * _strdup - does something
+ * @str : string
+ * Return: char
  */
+char *_strdup(char *str)
+{
+	char *array;
+	int len = 0;
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[len] != '\0')
+		len++;
+	array = malloc((len + 1) * sizeof(char));
+	if (array == NULL || len == 0)
+		return (NULL);
+	while (i <= len + 1)
+	{
+		array[i] = str[i];
+		i++;
+	}
+	return (array);
+}
