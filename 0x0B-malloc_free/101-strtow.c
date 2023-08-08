d18639f5ba8371c63c29d01f4367abbbd0ff183a
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-08-07 21:39:49 -0700

    strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,43 @@
 #include "main.h"
+int count(char *s);
 /**
- * - does something
- * @ : 
- * Return: 
+ * strtow - does something
+ * @str: string
+ * Return: char
  */
+char **strtow(char *str)
+{
+	char **arr, *tmp;
+	int len = 0, k = 0, w, c = 0, s, e;
+
+	while (*(str + len))
+		len++;
+	w = count(str);
+	if (w == 0)
+		return (NULL);
+	arr = (char **) malloc(sizeof(char *) * (w + 1));
+	
+}
+/**
+ * count - count
+ * @s: str
+ * Return: int
+ */
+int count(char *s)
+{
+	int f, c, w;
+
+	f = 0;
+	w = 0;
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			f = 0;
+		else if (f == 0)
+		{
+			f = 1;
+			w++;
+		}
+	}
+	return (w);
+}
