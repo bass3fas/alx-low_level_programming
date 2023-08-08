269e3d8bb12d84f77cc3d3e2ce299a1b28f04d56
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-08-08 05:45:29 -0700

    fix

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,32 +8,38 @@ int count(char *s);
 char **strtow(char *str)
 {
 	char **arr, *tmp;
-	int len = 0, k = 0, w, c = 0, s = 0, i, j;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
 	while (*(str + len))
 		len++;
-	w = count(str);
-	if (w == 0)
+	words = count(str);
+	if (words == 0)
 		return (NULL);
-	arr = (char **) malloc(sizeof(char *) * (w + 1));
+
+	arr = (char **) malloc(sizeof(char *) * (words + 1));
 	if (arr == NULL)
 		return (NULL);
+
 	for (i = 0; i <= len; i++)
 	{
-		if ((str[i] == ' ' || str[i] == '\0') && c != 0)
+		if (str[i] == ' ' || str[i] == '\0')
 		{
-			tmp = (char *) malloc(sizeof(char) * (c + 1));
-			if (tmp == NULL)
-				return (NULL);
-			for (j = 0; j < c; j++)
-				tmp[j] = str[s + j];
-			tmp[c] = '\0';
-			arr[k] = tmp;
-			k++;
-			c = 0;
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				arr[k] = tmp - c;
+				k++;
+				c = 0;
+			}
 		}
 		else if (c++ == 0)
-			s = i;
+			start = i;
 	}
 	arr[k] = NULL;
 	return (arr);
@@ -46,7 +52,6 @@ char **strtow(char *str)
 int count(char *s)
 {
 	int f, c, w;
-
 	f = 0;
 	w = 0;
 	for (c = 0; s[c] != '\0'; c++)
