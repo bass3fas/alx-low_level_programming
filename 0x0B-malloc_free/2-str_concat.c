d0a3a2ded87c7b61ac0303d864f0d073f5113ec5
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-08-09 00:36:31 -0700

    checking string not null should be before checking the first character

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,32 +12,24 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 	char *s3;
 
-	while (s1[len1] && s1)
+	while (s1 && s1[len1])
 		len1++;
-	while (s2[len2] && s2)
+	while (s2 && s2[len2])
 		len2++;
 	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (s3 == NULL)
+	if (!s3)
 		return (NULL);
 	i = 0;
 	j = 0;
-	if (s1)
-	{
+
 		while (i < len1)
 		{
 			s3[i] = s1[i];
 			i++;
 		}
-	}
-	if (s2)
-	{
+
 		while (i < (len1 + len2))
-		{
-			s3[i] = s2[j];
-			j++;
-			i++;
-		}
-	}
+			s3[i++] = s2[j++];
 	s3[i] = '\0';
 	return (s3);
 }
