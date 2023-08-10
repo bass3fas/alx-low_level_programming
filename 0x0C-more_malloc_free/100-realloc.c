d3e13aeaf8af098887f3f2be7397f930617ff976
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-08-10 12:18:51 -0700

    fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -31,7 +31,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		for (i = 0; i < new_size; i++)
 			ptr1[i] = old_ptr[i];
 	}
-	else
+	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
 			ptr1[i] = old_ptr[i];
