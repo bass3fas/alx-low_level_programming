1022c1e29a81f570c2af3cb96afe3385b45282af
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-08-15 23:35:26 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - something
+ * @name: str
+ * @f: func
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (! name || !f)
+		return;
+	f(name);
+}
