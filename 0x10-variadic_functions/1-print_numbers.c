bd25faa08647c6679643b8230544cc3f931b48c0
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-08-17 09:36:53 -0700

    betty

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,7 +9,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list list;
 
-	va_start(list,n);
+	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
 		if (!separator)
