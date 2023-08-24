eae88905b3e71f09b57f61b72b978fa514c50853
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-08-23 23:52:40 -0700

    intialize header

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - something
+ * @h: pointer
+ * Return: int
+ */
+size_t print_list(const list_t *h)
+{
+	size_t s = 0;
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u]%s\n", h->len, h->str);
+		h = h->next;
+		s++;
+	}
+	return (s);
+}
