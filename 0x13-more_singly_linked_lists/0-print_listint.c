9e22a408b317dcd408070860b8122a4482f00025
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-08-28 04:20:24 -0700

    print 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - something
+ * @h: var
+ * Return: size
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
