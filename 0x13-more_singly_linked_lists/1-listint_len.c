d9d343ef5909ce5e92dd5e175b973d143e8d6f73
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-08-28 04:24:12 -0700

    len 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,6 +1,17 @@
 #include "lists.h"
 /**
- * - 
- * @ : 
- * Return
+ * listint_len - something
+ * @h: var
+ * Return: size
  */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
