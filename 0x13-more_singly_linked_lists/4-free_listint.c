181d7cb947a2e57b9a4019b3829495078fc8c7ac
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-08-30 00:38:42 -0700

    4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,6 +1,16 @@
 #include "lists.h"
 /**
- * - 
- * @ : 
- * Return
+ * free_listint - s
+ * @head: h
  */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
