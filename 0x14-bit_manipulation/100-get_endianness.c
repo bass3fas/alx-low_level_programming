90ade737d0a192e1b919cb7e31ba86e20a3fa4d5
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-09-01 11:38:04 -0700

    100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * get_endianness
+ * Return: 0 or 1
+ */
+int get_endianness(void)
+{
+	int num;
+
+	num = 1;
+	if(*(char *)&num == 1)
+		return (1);
+	else
+		return (0);
+}
