d963940b16a1a88fade79705f231bb91611c8633
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-09-01 11:40:16 -0700

    betty

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * get_endianness
+ * get_endianness - func
  * Return: 0 or 1
  */
 int get_endianness(void)
@@ -8,7 +8,7 @@ int get_endianness(void)
 	int num;
 
 	num = 1;
-	if(*(char *)&num == 1)
+	if (*(char *)&num == 1)
 		return (1);
 	else
 		return (0);
