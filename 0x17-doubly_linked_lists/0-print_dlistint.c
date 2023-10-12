6b8a51e620bc5d52e230bc9c5a6e92af0109b1cd
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-10-12 03:18:06 -0700

    0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,7 +10,11 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t i = 0;
 
 	temp = h;
-	while (temp->next)
+	if (temp == NULL)
+		return (i);
+	while(temp->prev)
+		temp = temp->prev;
+	while (temp)
 	{
 		printf("%d\n", temp->n);
 		temp = temp->next;
