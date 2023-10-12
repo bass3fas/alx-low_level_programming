c6ab2118afacfdb8c6c827781433a1b11f0cee79
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-10-12 03:09:48 -0700

    4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees all the elements of a dlistint_t list
+ * @h: head node poiter
+ */
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *current;
+
+    while (head != NULL) {
+        current = head;
+        head = head->next;
+        free(current);
+    }
+}
