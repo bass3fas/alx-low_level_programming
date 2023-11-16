839f93df6243feae6fdf20f8acbb0ff15321f470
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-11-16 02:30:01 -0800

    fix

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -19,7 +19,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (!first)
 				printf(", ");
-			printf("'%s' : '%s'", node->key, node->value);
+			printf("'%s':'%s'", node->key, node->value);
 			first = 0;
 			node = node->next;
 		}
