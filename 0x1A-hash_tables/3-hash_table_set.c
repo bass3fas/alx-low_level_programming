f4a540baf0df3f6ec46b0ee230d428e9112fc2c3
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-11-16 00:47:38 -0800

    check

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,7 +24,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(temp->key, key) == 0)
 		{
+			free(temp->value);
 			temp->value = strdup(value);
+			if (temp->value == NULL)
+				return (0);
 			return (1);
 		}
 
